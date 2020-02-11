132f254bfa5402e6994ff48eb73531682e4a827d
SakshiSrivastava55 <53784511+SakshiSrivastava55@users.noreply.github.com>
2020-02-11 12:13:25 +0530

    Add files via upload

---
+++ labb.c
@@ -0,0 +1,60 @@
+#include<stdio.h>
+void search(int arr[],int key,int n)
+{
+	int mid,last,beg;
+	int i,j,count=0,k;
+	beg=0;
+	last=n-1;
+	mid=(beg+last)/2;
+	while(beg<=last)
+	{
+		if(arr[mid]<key)
+		beg=mid+1;
+		else if(arr[mid]==key)
+		{	break;
+		}
+		else
+			last=mid-1;
+		
+		mid=(beg+last)/2;
+	}
+	 	if(beg>last)
+	 	{
+	 	printf("-1-10\n");
+	 	return;
+	 	
+	 	}	
+		i=beg;
+		for(i=beg;i<last;i++)
+		{
+			if(arr[i]==key)
+			{
+			printf("FIRST OCCURANCE:%d\n",i);
+			break;
+			}
+		}
+		j=i-1;
+		for(k=i;k<n;k++)
+		{
+			if(key==arr[i])
+			{   j++;
+			    count++;
+			}
+		}
+		printf("LAST OCCURANCE:%d\n",j);
+		printf("COUNT:%d\n",count);
+}				
+
+void main()
+{
+	int a[10],i,k,l;
+	printf("ENTER THE SIZE OF THE ARRAY\n");
+	scanf("%d",&l);
+	printf("ENTER ELEMENTS IN THE SORTED MANNER\n");
+	for(i=0;i<l;i++)
+	scanf("%d",&a[i]);
+	printf("ENTER ELEMENTS TO BE SEARCHED\n");
+	scanf("%d",&k);
+	search(a,k,l);
+}
+	
